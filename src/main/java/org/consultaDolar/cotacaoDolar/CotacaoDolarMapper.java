3935346763f4e55df33d533bc85db834b981ceac
Felipe Escórcio <felipe.escorciosousa@gmail.com>
2023-08-11 23:35:39 -0300

    feat: creates mapper

---
+++ src/main/java/org/consultaDolar/cotacaoDolar/CotacaoDolarMapper.java
@@ -0,0 +1,19 @@
+package org.consultaDolar.cotacaoDolar;
+
+import org.consultaDolar.cotacaoDolar.repository.entity.CotacaoDolarEntity;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "cdi")
+public interface CotacaoDolarMapper {
+
+    List<CotacaoDolar> toDomainList(List<CotacaoDolarEntity> cotacaoDolarList);
+
+    CotacaoDolar toDomain(CotacaoDolarEntity cotacaoDolar);
+
+    @InheritInverseConfiguration(name = "toDomain")
+    CotacaoDolarEntity toEntity(CotacaoDolar cotacaoDolar);
+
+}
