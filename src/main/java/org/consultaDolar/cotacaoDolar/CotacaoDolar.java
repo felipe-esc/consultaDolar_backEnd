a931dd4be57b4b8258a8b1089baffdfb80167c40
Felipe Escórcio <felipe.escorciosousa@gmail.com>
2023-08-11 23:34:27 -0300

    feat: creates cotacaoDolar

---
+++ src/main/java/org/consultaDolar/cotacaoDolar/CotacaoDolar.java
@@ -0,0 +1,24 @@
+package org.consultaDolar.cotacaoDolar;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+
+import java.sql.Timestamp;
+
+@Data
+public class CotacaoDolar {
+
+    @JsonIgnore
+    private Integer requestId;
+
+    private Timestamp requestTimestamp;
+
+    private String dataCotacao;
+
+    private Double cotacaoCompra;
+
+    private Double cotacaoVenda;
+
+    private Timestamp dataHoraCotacao;
+
+}
