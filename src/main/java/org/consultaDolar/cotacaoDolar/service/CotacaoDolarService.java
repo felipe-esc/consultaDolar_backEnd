a5628d01fa052ead7532c1b33362236dfd10ad7b
Felipe Escórcio <felipe.escorciosousa@gmail.com>
2023-08-11 23:54:47 -0300

    feat: creates CotacaoDolarService

---
+++ src/main/java/org/consultaDolar/cotacaoDolar/service/CotacaoDolarService.java
@@ -0,0 +1,28 @@
+package org.consultaDolar.cotacaoDolar.service;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.transaction.Transactional;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.consultaDolar.cotacaoDolar.CotacaoDolar;
+import org.consultaDolar.cotacaoDolar.CotacaoDolarMapper;
+import org.consultaDolar.cotacaoDolar.repository.CotacaoDolarRepository;
+
+@ApplicationScoped
+@AllArgsConstructor
+@Slf4j
+public class CotacaoDolarService {
+
+    private final CotacaoDolarRepository cotacaoDolarRepository;
+
+    private final CotacaoDolarMapper cotacaoDolarMapper;
+
+    @Transactional
+    public void save(@NotNull CotacaoDolar cotacaoDolar) {
+        log.info("Salvando cotacao:" + cotacaoDolar);
+        cotacaoDolarRepository.persist(
+                cotacaoDolarMapper.toEntity(cotacaoDolar)
+        );
+    }
+}
